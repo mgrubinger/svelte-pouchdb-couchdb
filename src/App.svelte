28e3dab6069fe3bf04b7685e585da3ecf4e091e4
Martin Grubinger <martin.grubinger@gmail.com>
2019-05-24 09:29:40 +0200

    refactor to eats. add header

--- src/App.svelte
+++ src/App.svelte
@@ -3,81 +3,86 @@
   import { sortBy } from 'lodash'
   import PouchDB from 'pouchdb-browser'
 
-  import TodoItem from './todo-item.svelte'
+  import Header from './header.svelte'
+  import EatItem from './eat-item.svelte'
 
   // Set up local PouchDB and continuous replication to remote CouchDB
   let db = new PouchDB('db')
-  const replication = PouchDB.sync('db', 'http://localhost:5984/svelte-todo-db', {
-    live: true,
-    retry: true
-  }).on('change', async function (info) {
-    await updateTodos()
-  }).on('error', function (err) {
-    console.log('Replication error:', err)
-  })
+  // const replication = PouchDB.sync('db', 'http://localhost:5984/svelte-todo-db', {
+  //   live: true,
+  //   retry: true
+  // }).on('change', async function (info) {
+  //   await updateEats()
+  // }).on('error', function (err) {
+  //   console.log('Replication error:', err)
+  // })
 
   // Set up our vars and defaults
-  let newTodoText = ''
+  let newEatText = ''
+  let newEatDistance = ''
+  let newEatPrice = ''
   let sortByWhat = 'createdAt'
   let filterByWhat = ''
   let isLoading = true
+
   // All the todos directly from the PouchDB. Sorting and filtering comes later
-  let todos = []
-  $: sortedAndFilteredTodos = sortBy(todos, [sortByWhat]).filter((todo) => {
+  let eats = []
+  $: sortedAndFilteredEats = sortBy(eats, [sortByWhat]).filter((eat) => {
     const [filterKey, filterValue] = filterByWhat.split(':')
     // Only filter if there’s a proper filter set
-    return filterKey && filterValue ? todo[filterKey].toString() === filterValue : true
+    return filterKey && filterValue ? eat[filterKey].toString() === filterValue : true
   })
 
   // Helper for reloading all todos from the local PouchDB. It’s on-device and has basically zero latency,
   // so we can use it quite liberally instead of keeping our local state up to date like you’d do
   // in a Redux reducer. It also saves us from having to rebuild the local state todos from the data we sent
   // to the database and the `_id` and `_rev` values that were sent back.
-  async function updateTodos() {
+  async function updateEats() {
     const allDocs = await db.allDocs({
       include_docs: true
     })
-    todos = allDocs.rows.map(row => row.doc)
+    eats = allDocs.rows.map(row => row.doc)
     isLoading = false
   }
 
   // Event handlers for adding, updating and removing todos
   async function add(event) {
-    const newTodo = {
-      text: newTodoText,
-      complete: false,
+    const newEat = {
+      text: newEatText,
+      distance: newEatDistance,
+      price: newEatPrice,
       createdAt: new Date().toISOString()
     }
-    const addition = await db.post(newTodo)
+    const addition = await db.post(newEat)
     if (addition.ok) {
-      await updateTodos()
+      await updateEats()
     }
-    newTodoText = ''
+    newEatText = ''
   }
 
   async function updateStatus(event) {
-    const { todo } = event.detail
-    const update = await db.put(todo)
+    const { eat } = event.detail
+    const update = await db.put(eat)
     if (update.ok) {
-      await updateTodos()
+      await updateEats()
     }
   }
 
   async function removeItem(event) {
-    const { todo: todoToRemove } = event.detail
-    const removal = await db.remove(todoToRemove)
+    const { eat: eatToRemove } = event.detail
+    const removal = await db.remove(eatToRemove)
     if (removal.ok) {
       // For removal, we can just update the local state instead of reloading everything from PouchDB,
       // since we no longer care about the doc’s revision.
-      todos = todos.filter((todo) => {
-        return todo._id !== todoToRemove._id
+      eats = eats.filter((eat) => {
+        return eat._id !== eatToRemove._id
         })
     }
   }
 
   // Load todos on first run
   onMount(async () => {
-    await updateTodos()
+    await updateEats()
   })
 </script>
 
@@ -94,18 +99,20 @@
   }
 </style>
 
+<Header>
+
 {#if isLoading}
   <h1>
-    Loading your todos…
+    Loading your eats...
   </h1>
 {:else}
-  {#if todos.length === 0}
+  {#if eats.length === 0}
     <h1>
-      Zero Todos! Nice ✊
+      Zero Eats! So hungry 🥙
     </h1>
   {:else}
     <h1>
-      Showing {sortedAndFilteredTodos.length} of {todos.length} todos
+      Showing {sortedAndFilteredEats.length} of {eats.length} eats
     </h1>
   {/if}
 {/if}
@@ -114,26 +121,32 @@
   <label>Sort by:</label>
   <select bind:value={sortByWhat}>
     <option value='createdAt'>Time</option>
-    <option value='text'>Todo text</option>
-    <option value='complete'>Completion</option>
-  </select>
-</div>
-<div>
-  <label>Filter:</label>
-  <select bind:value={filterByWhat}>
-    <option value=''>Show all todos</option>
-    <option value='complete:true'>Show completed todos</option>
-    <option value='complete:false'>Show open todos</option>
+    <option value='distance'>Distance</option>
+    <option value='price'>Price</option>
   </select>
 </div>
 
+
 <ul>
-  {#each sortedAndFilteredTodos as todo (todo._id)}
-    <TodoItem todo={todo} on:remove={removeItem} on:update={updateStatus}/>
+  {#each sortedAndFilteredEats as eat (eat._id)}
+    <EatItem eat={eat} on:remove={removeItem} on:update={updateStatus}/>
   {/each}
 </ul>
 
 <form on:submit|preventDefault={add}>
-  <input type='text' bind:value={newTodoText}>
-  <button type='submit'>➕ Add new task</button>
+  <input type='text' bind:value={newEatText}>
+
+  <select bind:value={newEatDistance}>
+    <option value='near'>Near</option>
+    <option value='medium'>Medium</option>
+    <option value='far'>Far</option>
+  </select>
+
+  <select bind:value={newEatPrice}>
+    <option value='cheap'>Cheap</option>
+    <option value='pricey'>Pricey</option>
+  </select>
+
+  <button type='submit'>➕ Add new eat</button>
 </form>
+</Header>
---
+++ src/header.svelte
@@ -0,0 +1,48 @@
+<script>
+import {random} from 'lodash'
+
+let food = ["🥙", "🍔", "🥗", "🥪", "🥯", "🍣", "🍲", "🍌", "🌭", "🥑", "🌯"]
+$: currentFood = "";
+
+
+function rand(timeout) {
+
+    if(timeout > 500) {
+        console.log("stop");
+        return;
+    } 
+    window.setTimeout(() => {
+        currentFood = food[random(0, food.length-1)];
+        timeout *= 1.08;
+        rand(timeout)
+    }, timeout);
+}
+// kick it off
+rand(10);
+
+</script>
+
+<style>
+
+:global(body) {
+    padding: 0;
+    margin: 0;
+}
+
+main {
+    max-width: 700px;
+    margin: 0 auto;
+    text-align: center;
+}
+
+</style>
+
+<main>
+<h1>
+What are we going to eat today?<br/>
+{currentFood}
+</h1>
+
+
+<slot></slot>
+</main>
